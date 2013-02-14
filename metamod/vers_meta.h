96b24ab3f950d1768df33cb64b41a29730d6a012
David Anderson <dvander@alliedmods.net>
2013-02-13 23:26:41 -0800

    Bump version.

--- metamod/vers_meta.h
+++ metamod/vers_meta.h
@@ -48,8 +48,8 @@
 # endif /* _MSC_VER */
 #endif /* not OPT_TYPE */
 
-#define VDATE			"2013-02-11"
-#define VVERSION		"1.20"
+#define VDATE			"2013-02-14"
+#define VVERSION		"1.20-am"
 #define RC_VERS_DWORD	1,20,0,0	// Version Windows DLL Resources in res_meta.rc
 
 #endif /* VERS_META_H */
